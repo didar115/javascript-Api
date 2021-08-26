4753128ca6e6abc87ba8a9a65d2a333bdfd4f2b9
didar <dider.alam115@gmail.com>
2021-08-26 23:12:50 +0600

    get user buddy from api which is a nested api

--- app.js
+++ app.js
@@ -10,3 +10,38 @@ const displayQuote = quote => {
     quoteElement.innerHTML = quote.quote;
     
 }
+
+
+
+
+
+// collect user name and email from an api 
+
+
+const userbuddy = () => {
+    fetch("https://randomuser.me/api/?results=10")
+        .then(res => res.json())
+        .then(data => displayBuddies(data));
+}
+
+
+const displayBuddies = data => {
+    
+    const divId = document.getElementById('quote-div');
+  console.log(data);
+    const buddies = data.results;
+    for (const frnd of buddies) {
+          const p = document.createElement("p");
+        p.innerText = `
+        Name: ${frnd.name.first} ${frnd.name.last}
+         Email: ${frnd.email}
+        `;
+        divId.appendChild(p);
+    }
+
+
+}
+
+userbuddy();
+
+
